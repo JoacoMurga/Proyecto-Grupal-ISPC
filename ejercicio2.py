83b2fde34eca63f78766d4127839cc148ca4a1cb
tsuem <tsuemotop@gmail.com>
2022-08-10 18:41:06 -0300

    ejercicio2 de python

---
+++ ejercicio2.py
@@ -0,0 +1,13 @@
+Dueno2 = [23546987,  "Maria",  "Perez", 4789689,  "Pueyrredon  811"]
+
+for i in range(1, 5):
+    if i == 0 or i == 2:
+        continue
+    print(Dueno2[i], end="\n")
+
+
+
+""" for dato in Dueno2:
+    if (dato == "Maria" or dato == 4789689 or dato == "Pueyrredon  811"):
+        print(dato, end="\n") """
+    
